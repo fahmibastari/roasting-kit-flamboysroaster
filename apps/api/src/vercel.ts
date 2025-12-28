67b445b4d596b6e9ac780af4b24466d67e38918e
fahmibastari <fahmibastari549@gmail.com>
2025-12-28 23:49:47 +0700

    Chore: Add Vercel Serverless Config for API

---
+++ apps/api/src/vercel.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const expressApp = express();
+
+const bootstrap = async () => {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    // Config sama seperti main.ts
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    app.enableCors();
+
+    await app.init();
+};
+
+bootstrap();
+
+export default expressApp;
