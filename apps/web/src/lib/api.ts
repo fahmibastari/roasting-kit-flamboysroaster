68260998aed8fcc159215ac331317c27a7d583d1
fahmibastari <fahmibastari549@gmail.com>
2026-01-05 13:19:22 +0700

    fix: add ngrok skip browser warning

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
+    headers: {
+        "ngrok-skip-browser-warning": "true"
+    }
 });
 
 // Request Interceptor: Pasang Token
